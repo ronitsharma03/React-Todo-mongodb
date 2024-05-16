0b9305bd55bcca7fb68ed794770007f3502aef30
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-05-17 04:04:18 +0530

    Signin route for user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,6 +9,8 @@ const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const JWT_SECRET = process.env.JWT_SECRET;
 
+
+
 router.get('/me', async (req, res) => {
     const token = req.headers.authorization;
 
@@ -77,6 +79,58 @@ router.post("/signup", async (req, res) => {
         return res.status(411).json({
             message: "Something went wrong!"
         });
+    }
+});
+
+// Signin route for the user
+const signinBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string().min(6)
+});
+
+router.post("/signin", async (req, res) => {
+    const { success } = signinBody.safeParse(req.body);
+    if (!success) {
+        return res.status(411).json({
+            message: "Inputs are wrong!"
+        });
+    }
+    else {
+        try {
+            const loggedUser = await User.findOne({
+                username: req.body.username,
+            });
+            
+            if(!loggedUser){
+                return res.status(411).json({
+                    message: "User not found!"
+                });
+            }
+
+            const isPassword = await bcrypt.compare(req.body.password, loggedUser.password);
+            if(!isPassword){
+                return res.status(411).json({
+                    message: "Error while logging in: Incorrect password"
+                });
+            }
+
+            
+            const token = jwt.sign({
+               userId: loggedUser._id 
+            }, JWT_SECRET);
+
+            res.status(200).json({
+                message: "User Authenticated!",
+                token: token,
+                firstname: loggedUser.firstname
+            });
+        }catch(e){
+            console.log(e);
+            return res.status(411).json({
+                message: "Signin route catch - Something went wrong!"
+            })
+        }
+        
     }
 });
 
