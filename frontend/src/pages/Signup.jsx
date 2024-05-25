1e1d0734bf312a9f1d266e632c2a2fe85ff6e42a
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-05-25 09:56:30 +0530

    Added components on signup page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,10 +1,21 @@
+import { BottomWarning } from "../components/Bottomwarning"
 import { Heading } from "../components/Heading"
+import { Input } from "../components/Input"
+import { Subheading } from "../components/Subheading"
 
 export const Signup = () => {
     return (
         <div>
             <div>
                 <div>
+                    <Heading />
+                    <Subheading />
+                    <Input />
+                    <Input />
+                    <Input />
+                    <Input />
+                    <Button />
+                    <BottomWarning />
                     
                 </div>
             </div>
