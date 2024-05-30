48aef3370a2dac403ef6d87585ce07b18036a24b
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-05-31 01:56:41 +0530

    Toast added for logout

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,10 +1,24 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { IconListTodo } from "./Icon";
+import toast, { Toaster } from "react-hot-toast";
 
 
 export const Appbar = (
     { user }
 ) => {
+    const navigate = useNavigate();
+    const logout = async () => {
+        toast.loading("Logging out...", {
+            id: "logout"
+        });
+        localStorage.clear();
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        toast.success("Logged out", {
+            id: "logout"
+        });
+        navigate("/signin");
+    }
+
     return (
         <header className="w-full flex flex-row items-center justify-between px-10 py-3 bg-blue-600">
             <div className="flex items-center gap-5">
@@ -19,11 +33,13 @@ export const Appbar = (
                         <div className="text-white text-xl">
                             {"Welcome, " + user}
                         </div>
-                        <button className="bg-white px-7 py-2 rounded-lg outline-none">Logout</button>
+                        <button onClick={logout} className="bg-white px-7 py-2 rounded-lg outline-none">Logout</button>
                     </ul>
                 </nav>
             </div>
-
+        <Toaster 
+            position="top-center"
+        />
         </header>
     )
 }
