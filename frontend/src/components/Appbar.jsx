39cf66d2a5225f9085845e0aa79f668a2084505d
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-04 23:42:24 +0530

    Adding responsiveness

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -22,24 +22,29 @@ export const Appbar = (
     return (
         <header className="w-full flex flex-row items-center justify-between px-10 py-3 bg-blue-600">
             <div className="flex items-center gap-5">
-                <Link draggable={false} to={"/dashboard"}><IconListTodo /></Link>
-                <div className="text-white text-2xl font-semibold">
-                    Planify
-                </div>
+                <Link draggable={false} to={"/dashboard"}>
+                    <div className="flex items-center gap-5">
+                        <IconListTodo />
+                        <div className="max-sm:hidden text-white text-[1.6em] font-semibold tracking-wide">
+                            Planify
+                        </div>
+                    </div>
+                </Link>
+
             </div>
             <div>
                 <nav className="">
                     <ul className="flex items-center gap-10">
-                        <div className="text-white text-xl">
+                        <div className="text-white text-xl tracking-wide">
                             {"Welcome, " + user}
                         </div>
-                        <button onClick={logout} className="bg-white px-7 py-2 rounded-lg outline-none">Logout</button>
+                        <button onClick={logout} className="bg-white px-7 py-2 rounded-lg outline-none tracking-wide">Logout</button>
                     </ul>
                 </nav>
             </div>
-        <Toaster 
-            position="top-center"
-        />
+            <Toaster
+                position="top-center"
+            />
         </header>
     )
 }
