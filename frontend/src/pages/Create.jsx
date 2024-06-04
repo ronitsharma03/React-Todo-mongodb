198e099d8cffb789cd8def9d9be5285321cf9f44
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-05 02:04:43 +0530

    bug fix

--- frontend/src/pages/Create.jsx
+++ frontend/src/pages/Create.jsx
@@ -2,21 +2,28 @@ import { useEffect, useRef, useState } from "react";
 
 export const Create = ({ forTitle, forDescription, clickDone }) => {
     const [isVisible, setIsVisible] = useState(false);
-    const inputRef = useRef(null);
+    const titleRef = useRef(null);
+    const descRef = useRef(null);
+    const divRef = useRef(null);
 
     useEffect(() => {
         const handleClickOutside = (event) => {
-            if (inputRef.current && !inputRef.current.contains(event.target)) {
+            if (
+                titleRef.current && 
+                !titleRef.current.contains(event.target) && 
+                !descRef.current.contains(event.target) &&
+                !divRef.current.contains(event.target)
+            ) {
                 handleCloseClick();
             }
-        }
+        };
         if (isVisible) {
-            document.addEventListener("click", handleClickOutside);
+            document.body.addEventListener("click", handleClickOutside);
         }
 
         return () => {
-            document.removeEventListener("click", handleClickOutside);
-        }
+            document.body.removeEventListener("click", handleClickOutside);
+        };
     }, [isVisible]);
 
     const handleInputClick = () => {
@@ -24,14 +31,14 @@ export const Create = ({ forTitle, forDescription, clickDone }) => {
     };
 
     const handleCloseClick = () => {
-        inputRef.current.value = "";
+        titleRef.current.value = "";
         setIsVisible(false);
     };
 
     return (
-        <div className="flex flex-col w-[30%] bg-transparent border border-slate-600 rounded-xl transition-all duration-300">
+        <div ref={divRef} className="flex flex-col w-[30%] bg-transparent border border-slate-600 rounded-xl transition-all duration-300">
             <input
-                ref={inputRef}
+                ref={titleRef}
                 onClick={handleInputClick}
                 onChange={forTitle}
                 className="bg-transparent p-3 outline-none text-white"
@@ -39,6 +46,7 @@ export const Create = ({ forTitle, forDescription, clickDone }) => {
             />
             <div className={`overflow-hidden transition-all duration-300 ${isVisible ? 'max-h-screen opacity-100 mt-4' : 'max-h-0 opacity-0 mt-0'}`}>
                 <input
+                ref={descRef}
                     onChange={forDescription}
                     className="bg-transparent p-5 outline-none text-white"
                     placeholder="Description"
@@ -46,11 +54,11 @@ export const Create = ({ forTitle, forDescription, clickDone }) => {
                 <div className="flex items-center justify-end gap-2 pb-2 mr-5">
                     <button
                         onClick={handleCloseClick}
-                        className="bg-transparent text-white cursor-pointer px-4 py-1 hover:bg-slate-800 text-sm"
+                        className="text-white cursor-pointer px-4 py-1 hover:bg-slate-800 text-sm"
                     >
                         Close
                     </button>
-                    <button onClick={clickDone} className="bg-transparent text-white cursor-pointer px-4 py-1 hover:bg-slate-800 text-sm">
+                    <button onClick={clickDone} className="text-white cursor-pointer px-4 py-1 hover:bg-slate-800 text-sm">
                         Done
                     </button>
                 </div>
