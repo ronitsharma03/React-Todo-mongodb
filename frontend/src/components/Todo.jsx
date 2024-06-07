20d73154f71919e7497eb9c5eca89dd00e240613
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-07 22:50:15 +0530

    UI fixes

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 
 export const Todo = ({ title, description, Date, marked }) => {
@@ -11,15 +12,20 @@ export const Todo = ({ title, description, Date, marked }) => {
 
     const formattedDate = formatDate(createdAt);
 
+    const [isClicked, setClicked] = useState(false);
+    const handleclick = () => {
+        setClicked(!isClicked);
+    }
 
     return (
-        <div className={`bg-transparent w-62 h-60 rounded-md border border-slate-800 overflow-hidden ${marked ? 'opacity-50' : ''}`}>
+        <div className={`bg-blue-100 dark:bg-transparent w-62 h-60 rounded-md border border-slate-800 overflow-hidden ${marked ? 'opacity-50' : ''} hover:cursor-pointer`} onClick={handleclick}>
             <div className="h-full grid grid-rows-8 grid-cols-4">
 
                 <div className="flex justify-between px-2 py-2 col-span-4 row-span-1">
-                    <input className="bg-transparent rounded-full checked" type="checkbox" />
-                    <div className="text-slate-600 mr-2">
-                        {formattedDate} {marked + ""}
+                    <input className="bg-transparent rounded-full" type="checkbox" />
+                    <div className="text-slate-600 mr-2 flex gap-5">
+                        {time} - {formattedDate}
+
                     </div>
                 </div>
                 <div className="px-2 py-3 w-full col-span-4 row-span-6 cursor-pointer">
@@ -32,7 +38,9 @@ export const Todo = ({ title, description, Date, marked }) => {
                     </div>
                 </div>
                 <div className="text-slate-600 place-self-end col-span-4 px-3 row-span-1">
-                    {time}
+                    <div>
+                        {marked ? "Completed" : "Pending"}
+                    </div>
                 </div>
 
             </div>
