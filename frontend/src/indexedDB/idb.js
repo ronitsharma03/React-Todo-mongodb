7b89157aa5446e123fdf0eef0cc0a304d11c8986
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-07-07 12:06:14 +0530

    initialized indexeddb

--- frontend/src/indexedDB/idb.js
+++ frontend/src/indexedDB/idb.js
@@ -1,21 +1,20 @@
-import { openDB } from "idb";
+import { openDB } from 'idb';
 
-const DB_NAME = "TaskWhiz_DB";
-const STORE_NAME = "taskStore";
+const DB_NAME = 'tasksDB';
+const STORE_NAME = 'tasks';
 const DB_VERSION = 1;
 
 export const initDB = async () => {
   const db = await openDB(DB_NAME, DB_VERSION, {
     upgrade(db) {
       if (!db.objectStoreNames.contains(STORE_NAME)) {
-        const store = db.createObjectStore(STORE_NAME, {
-          keyPath: "id",
-          autoIncrement: true,
-        });
-        store.createIndex("title", "title", { unique: false });
-        store.createIndex("completed", "completed", { unique: false });
+        const store = db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true });
+        store.createIndex('title', 'title', { unique: false });
+        store.createIndex('description', 'description', { unique: false });
+        store.createIndex('date', 'date', { unique: false });
+        store.createIndex('marked', 'marked', { unique: false });
       }
-    },
+    }
   });
   return db;
 };
@@ -27,15 +26,20 @@ export const getAllTasks = async () => {
 
 export const addTask = async (task) => {
   const db = await initDB();
+  task.needsSync = !navigator.onLine;
   return db.add(STORE_NAME, task);
 };
 
 export const updateTask = async (task) => {
   const db = await initDB();
+  task.needsSync = !navigator.onLine;
   return db.put(STORE_NAME, task);
-}
+};
 
 export const deleteTask = async (id) => {
   const db = await initDB();
-  return db.delete(STORE_NAME, id);
-}
+  const task = await db.get(STORE_NAME, id);
+  task.needsSync = !navigator.onLine;
+  task.deleted = true;
+  return db.put(STORE_NAME, task);
+};
